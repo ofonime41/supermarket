917c8bed6b909a176bc585d7c9593b7c94b49559
ofonime41 <ofonime41@gmail.com>
2023-10-19 10:47:57 +0100

    .nuxt made visible

---
+++ .output/server/chunks/app/_nuxt/test-styles.49c71b86.mjs
@@ -0,0 +1,6 @@
+const test_vue_vue_type_style_index_0_scoped_ca836897_lang = "#freshSlide .splide__arrow svg[data-v-ca836897]{fill:#0e0e0e;height:10rem;width:10rem}.splide__track[data-v-ca836897]{border:0;border-radius:0}#innerslide1 .splide__pagination__page.is-active[data-v-ca836897]{background:#f9f611;transform:scale(1.4);z-index:1}.splide__arrow[data-v-ca836897]{background:transparent;box-sizing:border-box;opacity:1}";
+
+const testStyles_49c71b86 = [test_vue_vue_type_style_index_0_scoped_ca836897_lang, test_vue_vue_type_style_index_0_scoped_ca836897_lang];
+
+export { testStyles_49c71b86 as default };
+//# sourceMappingURL=test-styles.49c71b86.mjs.map
---
+++ .output/server/chunks/app/styles.mjs
@@ -0,0 +1,11 @@
+const interopDefault = r => r.default || r || [];
+const styles = {
+  "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.16d77659.mjs').then(interopDefault),
+  "pages/index.vue": () => import('./_nuxt/index-styles.64874506.mjs').then(interopDefault),
+  "pages/test.vue": () => import('./_nuxt/test-styles.49c71b86.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.5437d746.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.ee00b7aa.mjs').then(interopDefault)
+};
+
+export { styles as default };
+//# sourceMappingURL=styles.mjs.map
